8cf88b75c8d8c4737db022298af890c525088629
mdcruz <marie.cruz1@news.co.uk>
2020-04-11 19:15:40 +0100

    update test

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -1,29 +1,14 @@
 /// <reference types="cypress" />
 
 describe('TodoApp', () => {
-  before(() => {
-    cy.visit('/');
-    cy.get('.new-todo').type('do lunch and learn about Cypress {enter}');
-    cy.get('.new-todo').type('have lunch {enter}');
-  });
-
   it('should add a new todo successfully', () => {
-    cy.getFirstTodoItem().should(
-      'be.equal',
-      'do lunch and learn about Cypress'
-    );
+    cy.visit('/');
+    cy.get('.new-todo').as('addTodo');
 
-    cy.verifyTodoItems('2 items');
-  });
+    cy.get('@addTodo').type('do lunch and learn about Cypress {enter}');
+    cy.get('@addTodo').type('have lunch {enter}');
 
-  it('should mark a todo item as completed', () => {
-    cy.get('.toggle')
-      .first()
-      .click();
-
-    cy.verifyTodoItems('1 item');
-
-    cy.contains('Completed').click();
+    cy.get('.todo-list').find('li').should('have.length', 2);
     cy.getFirstTodoItem().should(
       'be.equal',
       'do lunch and learn about Cypress'
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,9 +5,3 @@ Cypress.Commands.add('getFirstTodoItem', () => {
     .first()
     .invoke('text');
 });
-
-Cypress.Commands.add('verifyTodoItems', todoItems => {
-  cy.get('.todo-count')
-    .invoke('text')
-    .should('be.equal', `${todoItems} left`);
-});
