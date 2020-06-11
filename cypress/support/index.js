74b1a81d536eaf7efe61799390b23b9c3f3ffff4
Marie Drake <mcruzdrake@gmail.com>
2020-06-11 20:52:35 +0100

    add support for cypress screenshots to be added on mochawesome report

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,20 +1,11 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import './commands';
+import addContext from 'mochawesome/addContext';
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+Cypress.on('test:after:run', (test, runnable) => {
+  if (test.state === 'failed') {
+    const screenshot = `${Cypress.config('screenshotsFolder')}/${
+      Cypress.spec.name
+    }/${runnable.parent.title} -- ${test.title} (failed).png`;
+    addContext({ test }, screenshot);
+  }
+});
