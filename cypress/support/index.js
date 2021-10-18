10160611718f4e9cebd7338e9d72c3a99f3478d5
rkrisztian <2960445+rkrisztian@users.noreply.github.com>
2021-10-18 19:50:08 +0200

    Support for Cypress 8
    I could not test these changes yet...

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,8 +3,11 @@ import addContext from 'mochawesome/addContext';
 
 Cypress.on('test:after:run', (test, runnable) => {
   if (test.state === 'failed') {
-    const screenshotDir = `${Cypress.config('screenshotsFolder')}/${Cypress.spec.name}`;
-    const screenshot = `${screenshotDir}/${runnable.titlePath().join(' -- ')} (failed).png`;
+    const screenshotDir = `../screenshots/${Cypress.spec.name}`;
+    const screenshotFileBaseName =
+      runnable.titlePath().join(' -- ') +
+      (test.hookName ? ` -- ${test.hookName} hook` : '');
+    const screenshot = `${screenshotDir}/${screenshotFileBaseName)} (failed).png`;
     addContext({ test }, screenshot);
   }
 });
