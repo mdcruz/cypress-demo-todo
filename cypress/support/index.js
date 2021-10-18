d90225aeeb9e8498d0773164b06c48e05691c25d
rkrisztian <2960445+rkrisztian@users.noreply.github.com>
2021-10-18 19:55:45 +0200

    Correct the previous commit (for Cypress 8)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,7 +3,9 @@ import addContext from 'mochawesome/addContext';
 
 Cypress.on('test:after:run', (test, runnable) => {
   if (test.state === 'failed') {
-    const screenshotDir = `../screenshots/${Cypress.spec.name}`;
+    // It can also be a relative dir like `../screenshots/${Cypress.spec.name}`
+    // Cypress.config('screenshotsFolder') is now an absolute path, so it no longer works.
+    const screenshotDir = `assets/${Cypress.spec.name}`;
     const screenshotFileBaseName =
       runnable.titlePath().join(' -- ') +
       (test.hookName ? ` -- ${test.hookName} hook` : '');
